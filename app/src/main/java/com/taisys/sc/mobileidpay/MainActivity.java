097a27ca7f83ad0ae24dc7a9d2fce95643f9414a
SunnySan <sunny1227@pchome.com.tw>
2018-04-24 18:37:14 +0800

    Initial commit

---
+++ app/src/main/java/com/taisys/sc/mobileidpay/MainActivity.java
@@ -0,0 +1,60 @@
+package com.taisys.sc.mobileidpay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_myid:
+                    return true;
+                case R.id.navigation_transactions:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setOnClickListener();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
+    }
+
+    private void setOnClickListener(){
+        Button b1 = (Button) findViewById(R.id.btnMainScan);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScanActivity.class));
+            }
+        });
+
+    }
+
+
+}
