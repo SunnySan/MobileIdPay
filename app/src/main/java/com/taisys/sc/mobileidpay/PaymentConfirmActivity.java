18c2f62976e4dc4963de8afbc8c96ae28d5fed38
SunnySan <sunny1227@pchome.com.tw>
2018-05-02 18:49:24 +0800

    office

--- app/src/main/java/com/taisys/sc/mobileidpay/PaymentConfirmActivity.java
+++ app/src/main/java/com/taisys/sc/mobileidpay/PaymentConfirmActivity.java
@@ -1,10 +1,17 @@
 package com.taisys.sc.mobileidpay;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,16 +23,25 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.squareup.picasso.Picasso;
+import com.taisys.oti.Card;
 
 import org.json.JSONObject;
 
 public class PaymentConfirmActivity extends AppCompatActivity {
     private static final String TAG = PaymentConfirmActivity.class.getSimpleName();
 
+    private Card mCard = new Card();
+    private ProgressDialog pg = null;
+    private Context myContext = null;
+
     private String goodsId = "";
     private TextView txtName, txtDescription, txtPrice;
     private ImageView imgGoodsImage;
     private Goods goods = null;
+
+    private String sTransactionID = "";
+    private String sPublicKey = "";
+    private String sRsaSignature = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +66,9 @@ public class PaymentConfirmActivity extends AppCompatActivity {
             finish();
         }
 
+        sTransactionID = utility.generateRequestId();
+        myContext = this;
+        setOnClickListener();
         // search the barcode
         searchBarcode(barcode);
 
@@ -61,6 +80,82 @@ public class PaymentConfirmActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mCard!=null){
+            mCard.CloseSEService();
+        }
+        super.onDestroy();
+    }
+
+    private void showWaiting(final String title, final String msg) {
+        disWaiting();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                pg = new ProgressDialog(myContext);
+                // }
+                pg.setIndeterminate(true);
+                pg.setCancelable(false);
+                pg.setCanceledOnTouchOutside(false);
+                pg.setTitle(title);
+                pg.setMessage(msg);
+                pg.show();
+            }
+        });
+    }
+
+    private void disWaiting() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (pg != null && pg.isShowing()) {
+                    pg.dismiss();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
+    }
+
+    private void setOnClickListener(){
+        Button b1 = (Button) findViewById(R.id.btnPcConfirm);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //顯示Progress對話視窗
+                // showWaiting(getString(R.string.pleaseWait), getString(R.string.msgCheckCardAvailability));
+                showWaiting(getString(R.string.pleaseWait), getString(R.string.msgCreatingSignature));
+                mCard.OpenSEService(myContext, "A000000018506373697A672D63617264",
+                        new Card.SCSupported() {
+
+                            @Override
+                            public void isSupported(boolean success) {
+                                if (success) {
+                                    //手機支援OTI
+                                    getCardInfo();
+                                } else {
+                                    disWaiting();
+                                    utility.showMessage(myContext, getString(R.string.msgDoesntSupportOti));
+                                }
+                            }
+                        });
+            }
+        });
+
+        Button b2 = (Button) findViewById(R.id.btnPcCancel);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                //sendAuthenticationResultToServer(false);
+            }
+        });
     }
 
     /**
@@ -115,18 +210,9 @@ public class PaymentConfirmActivity extends AppCompatActivity {
 
             if (goods != null) {
                 goodsId = goods.getId();
-                String s = "";
-                s = getString(R.string.txtGoodsName);
-                txtName.setText(s + "\n" + goods.getName());
-                s = getString(R.string.txtGoodsPrice);
-                txtPrice.setText(s + "\n" + "$" + goods.getPrice());
-                s = getString(R.string.txtGoodsDescription);
-                txtDescription.setText(s + "\n" + goods.getDescription());
-                /*
-                txtDirector.setText(goods.getDescription());
-                txtDuration.setText(goods.getPrice());
-                */
-                //GlideApp.with(this).load(goods.getPicture()).into(imgGoodsImage);
+                txtName.setText(goods.getName());
+                txtPrice.setText("$" + goods.getPrice());
+                txtDescription.setText(goods.getDescription());
                 Log.e(TAG, "image url: " + goods.getPicture());
                 Picasso.with(this).load(goods.getPicture()).placeholder(R.mipmap.ic_launcher).into(imgGoodsImage);
             } else {
@@ -172,5 +258,190 @@ public class PaymentConfirmActivity extends AppCompatActivity {
         }
 
     }
+
+
+    private void getCardInfo(){
+        //顯示Progress對話視窗
+        //utility.showToast(myContext, getString(R.string.msgReadCardInfo));
+        //showWaiting(getString(R.string.pleaseWait), getString(R.string.msgReadCardInfo));
+        Log.d("MobileIdAuthentication", "Get Card Info");
+        String res[] = mCard.GetCardInfo();
+        //disWaiting();
+        if (res != null && res[0].equals(Card.RES_OK)) {
+            verifyPinCode();
+        } else {
+            disWaiting();
+            utility.showMessage(myContext, getString(R.string.msgUnableToGetIccid));
+        }
+
+    }
+
+    private void verifyPinCode(){
+        Log.d("MobileIdAuthentication", "Verify PIN code");
+        EditText editTextPinCode = (EditText) findViewById(R.id.pcEnterPinCode);
+        String pinCode = editTextPinCode.getText().toString();
+        if (pinCode.length()==0){
+            disWaiting();
+            utility.showMessage(myContext,getString(R.string.msgPleaseEnterPinCode));
+            return;
+        }
+        //utility.showToast(myContext, getString(R.string.msgVerifyPinCode));
+        //showWaiting(getString(R.string.pleaseWait), getString(R.string.msgVerifyPinCode));
+        int pinId = 0x1;
+        pinCode = utility.byte2Hex(pinCode.getBytes());
+        String res = mCard.VerifyPIN(pinId, pinCode);
+        /*
+        if (mCard!=null){
+            mCard.CloseSEService();
+        }
+        */
+        if (res != null && res.equals(Card.RES_OK)) {
+            Log.d("MobileId", "PIN verification passed");
+            generateRSASignature();
+        } else {
+            disWaiting();
+            Log.d("MobileId", "PIN code compared failed, user enter PIN= " + pinCode + ", response= " + res);
+            //utility.showMessage(myContext, pinCode);
+            utility.showMessage(myContext, getString(R.string.msgPinCodeIsIncorrect));
+        }
+    }
+
+    //讀出PublicKey並產生 RSA 簽章
+    private void generateRSASignature(){
+        String res[] = null;
+
+        //讀出 public key
+        res = mCard.ReadFile(0x0201, 0x0, 264);
+        if (res != null && res[0].equals(Card.RES_OK)) {
+            sPublicKey = res[1];
+            Log.d(TAG, "public key=" + sPublicKey);
+        }else if (res[0]!=null && res[0].equals("-15")){    //key不存在，建立key
+            //產生 RSA key pair
+            String resString = mCard.GenRSAKeyPair(Card.RSA_1024_BITS, 0x0201, 0x0301);
+            if (resString != null && resString.equals(Card.RES_OK)) {
+                Log.d(TAG, "Gen key pair OK!");
+                //重新讀出 PublicKey
+                res = mCard.ReadFile(0x0201, 0x0, 264);
+                if (res != null && res[0].equals(Card.RES_OK)) {
+                    sPublicKey = res[1];
+                    Log.d(TAG, "public key=" + sPublicKey);
+                } else {
+                    disWaiting();
+                    if (mCard!=null){
+                        mCard.CloseSEService();
+                    }
+                    utility.showMessage(myContext, getString(R.string.msgFailToReadPublicKey) + "error code=" + res[0]);
+                    Log.e(TAG, "no public key:" + res[0]);
+                    return;
+                }
+
+            } else {
+                disWaiting();
+                if (mCard!=null){
+                    mCard.CloseSEService();
+                }
+                Log.e(TAG, "Gen key pair Failed! error code=" + resString);
+                utility.showMessage(myContext, getString(R.string.msgUnableToGenerateRsaKeyPair) + ", error code=" + resString);
+                return;
+            }
+        } else {
+            disWaiting();
+            if (mCard!=null){
+                mCard.CloseSEService();
+            }
+            utility.showMessage(myContext, getString(R.string.msgFailToReadPublicKey) + "error code=" + res[0]);
+            Log.e(TAG, "no public key:" + res[0]);
+            return;
+        }
+
+        res = mCard.RSAPriKeyCalc(sTransactionID, false, 0x0301);
+        if (mCard!=null){
+            mCard.CloseSEService();
+        }
+        if (res != null && res[0].equals(Card.RES_OK)) {
+            Log.d(TAG, "RSA signature successfully, signature hash=" + res[1]);
+            sRsaSignature = res[1];
+            sendAuthenticationResultToServer(true);
+        }else{
+            Log.d(TAG, "RSA signature failed！ error code=" + res[0]);
+            disWaiting();
+            utility.showMessage(myContext, getString(R.string.msgPinCodeIsIncorrect));
+        }
+
+    }
+
+    private void sendAuthenticationResultToServer(boolean bPass){
+        String iccid = utility.getMySetting(myContext, "iccid");
+        if (iccid==null || iccid.length()==0){
+            iccid = utility.generateRequestId();
+            utility.setMySetting(myContext, "iccid", iccid);
+        }
+
+        String sResult = "";
+        if (bPass) sResult="00000"; else sResult="99999";
+
+        // 建立請求物件，設定網址
+        String url = "http://cms.gslssd.com/MobileIdPayServer/ajaxAddNewTransaction.jsp?";
+        url += "iccid=" + iccid;
+        url += "&goodsId=" + goodsId;
+        url += "&transactionId=" + sTransactionID;
+        url += "&publicKey=" + sPublicKey;
+        url += "&signature=" + sRsaSignature;
+
+
+        //資料都有了，將資料送給 server
+        //showWaiting(getString(R.string.pleaseWait), getString(R.string.msgSendRegistrationRequest));
+        Log.e(TAG, "Sunny: search goods");
+        // making volley's json request
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        disWaiting();
+                        Log.e(TAG, "Sunny: server response: " + response.toString());
+
+                        try{
+                            String sResultCode = response.getString("resultCode");
+                            String sResultText = response.getString("resultText");
+                            if (sResultCode==null || sResultCode.length()<1 || !sResultCode.equals("00000")){
+                                if (sResultText==null || sResultText.length()<1){
+                                    utility.showMessage(myContext, getString(R.string.msgProcessFailed));
+                                }else{
+                                    utility.showMessage(myContext, sResultText);
+                                }
+                            }else{
+                                AlertDialog.Builder dialog = new AlertDialog.Builder(myContext);
+                                dialog.setTitle(R.string.msgSystemInfo)
+                                        .setMessage(getString(R.string.msgProcessSucceeded))
+                                        .setIcon(R.drawable.ic_launcher)
+                                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                                finish();
+                                            }
+                                        })
+                                        .show();
+                            }
+                        }catch (Exception e){
+                            utility.showMessage(myContext, getString(R.string.msgUnableToParseServerResponseData));
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                disWaiting();
+                Log.e(TAG, "Error: " + error.getMessage());
+                utility.showMessage(myContext, getString(R.string.msgProcessFailed) + ": " + error.getMessage());
+            }
+        });
+
+        MyApplication.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
 
 }
---
+++ app/src/main/java/com/taisys/sc/mobileidpay/utility.java
@@ -0,0 +1,97 @@
+package com.taisys.sc.mobileidpay;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class utility {
+    public static String getMySetting(Context context, String keyName){
+        // 建立SharedPreferences物件
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String valueString = sharedPreferences.getString(keyName, "");
+        return valueString;
+    }
+
+    public static void setMySetting(Context context, String keyName, String value){
+        // 建立SharedPreferences物件
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(keyName, value);
+        editor.apply();
+    }
+
+    public static void showToast(Context context, String msg) {
+        if (msg==null || msg.length()==0) return;
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+
+    public static void showMessage(Context context, String msg){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle(R.string.msgSystemInfo)
+                .setMessage(msg)
+                .setIcon(R.drawable.ic_launcher)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    //取得 byte array 每個 byte 的 16 進位碼
+    public static String byte2Hex(byte[] b) {
+        String result = "";
+        for (int i=0 ; i<b.length ; i++)
+            result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+        return result;
+    }
+
+    //將 16 進位碼的字串轉為 byte array
+    public static byte[] hex2Byte(String hexString) {
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i=0 ; i<bytes.length ; i++)
+            bytes[i] = (byte) Integer.parseInt(hexString.substring(2 * i, 2 * i + 2), 16);
+        return bytes;
+    }
+
+    /*********************************************************************************************************************/
+
+//產生20碼的RequestId
+    public static String generateRequestId(){
+        //以【日期+時間+四位數隨機數】作為送給BSC API的 RequestId，例如【20110816-102153-6221】
+        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyyMMdd-HHmmss");
+        java.util.Date currentTime = new java.util.Date();//得到當前系統時間
+        String txtRandom = String.valueOf(Math.round(Math.random()*10000));
+        txtRandom = MakesUpZero(txtRandom, 4);	//不足4碼的話，將前面補0
+        String txtRequestId = formatter.format(currentTime) + "-" + txtRandom; //將日期時間格式化，加上一個隨機數，作為RequestId，格式是yyyyMMdd-HHmmss-xxxx
+
+        return txtRequestId;
+    }
+
+/*********************************************************************************************************************/
+
+/*********************************************************************************************************************/
+
+    /**
+     * 數字不足部份補零回傳
+     * @param str 字串
+     * @param lenSize 字串數字最大長度,不足的部份補零
+     * @return 回傳補零後字串數字
+     */
+    public static String MakesUpZero(String str, int lenSize) {
+        String zero = "0000000000";
+        String returnValue = zero;
+
+        returnValue = zero + str;
+
+        return returnValue.substring(returnValue.length() - lenSize);
+
+    }
+
+/*********************************************************************************************************************/
+
+}
